05e92d092fbc30b9bf3562cc78a9f07516b646a7
Axel Demborg <axel.demborg@gmail.com>
2019-08-11 18:00:05 +0200

    store access token localy on server so that others can use the same one

--- main.py
+++ main.py
@@ -1,48 +1,72 @@
+import json
 from base64 import b64encode
+from pathlib import Path
+
+import requests
 from flask import Flask, redirect, request, url_for, jsonify
-from requests import post, get, put
+
+AUTH_FILE = Path('.auth')
+
 app = Flask(__name__)
 app.config.from_pyfile('settings_local.py')
 
-@app.route('/')
-def hello_world():
-    me = get(
-            'https://api.spotify.com/v1/me',
-            headers={'Authorization': 'Bearer {}'.format(
-                request.cookies.get('access_token'))}).json()
-    devices = get(
-            'https://api.spotify.com/v1/me/player/devices',
-            headers={'Authorization': 'Bearer {}'.format(
-                request.cookies.get('access_token'))}).json()
-    res = put(
-            "https://api.spotify.com/v1/me/player/volume?volume_percent=50",
-            headers={'Authorization': 'Bearer {}'.format(
-                request.cookies.get('access_token'))})
 
+def authorized_request(**kwargs) -> requests.Response:
+    if not AUTH_FILE.is_file():
+        raise FileNotFoundError
+    auth = json.loads(AUTH_FILE.read_text())
+    headers = kwargs.get('headers', {})
+    headers['Authorization'] = 'Bearer {}'.format(auth['access_token'])
+    kwargs['headers'] = headers
+
+    return requests.request(**kwargs)
+
+@app.route('/')
+def main():
+    try:
+        resp = authorized_request(
+                url='https://api.spotify.com/v1/me',
+                method='GET')
+    except FileNotFoundError:
+        return redirect(url_for('login'))
+    resp.raise_for_status()
+    me = resp.json()
+
+    resp = authorized_request(
+            url="https://api.spotify.com/v1/me/player/volume?volume_percent=50",
+            method='PUT')
+    
+    resp = authorized_request(
+            url='https://api.spotify.com/v1/me/player/devices',
+            method='GET')
+    resp.raise_for_status()
+    devices = resp.json()
 
     return devices
 
 @app.route('/login/')
 def login():
     auth_url = 'https://accounts.spotify.com/authorize?client_id={}&response_type=code&redirect_uri={}&scope={}'
-    return redirect(auth_url.format(app.config['CLIENT_ID'], 'http://localhost:5000/callback/', 'user-read-playback-state user-modify-playback-state'))
+    callback_url = request.url_root + 'callback'
+    return redirect(auth_url.format(app.config['CLIENT_ID'], callback_url, 'user-read-playback-state user-modify-playback-state'))
 
 @app.route('/callback/')
 def callback():
     code = request.args.get('code')
-    res = post(
+    res = requests.post(
             'https://accounts.spotify.com/api/token', 
             data={
                 'grant_type': 'authorization_code',
                 'code': code,
-                'redirect_uri': 'http://localhost:5000/callback/',
+                'redirect_uri': request.url_root + 'callback/',
                 'client_id': app.config['CLIENT_ID'],
                 'client_secret': app.config['CLIENT_SECRET'],
             }
             
         )
     data = res.json()
-    resp = redirect('http://localhost:5000/')
+    AUTH_FILE.write_text(json.dumps(data))
+    resp = redirect(url_for('main'))
     resp.set_cookie('access_token', data['access_token']) 
     resp.set_cookie('refresh_token', data['refresh_token']) 
     return resp
